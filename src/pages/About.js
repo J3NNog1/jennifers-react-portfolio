01f1a465849c5298ff8a521c45f7ca36fa3d12e1
jennifer j <jenniferjuarezjj@gmail.com>
2023-07-30 21:56:40 -0400

    add bio to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,15 @@ function About() {
     return (
         <main>
         <h1>About</h1>
-        <h2></h2>
+        <h2>I have always had a love for learning and trying new things, some more unorthodox than others ever since a young age. Learning new ways of thinking has always sparked my interest. Growing up in Brooklyn, New York I had the opportunity to be a part of a COED High School wrestling team where I found my “first calling.” I was set apart from a lot of my competitors because I was a girl in a male-dominated sport. With the help of my coaches and teammates, I was able to dominate my weight class, earning me a spot on the USA Women’s Wrestling team as well as a full ride to the college of Life University located in Atlanta, GA.
+
+Attending Life University was a life-changing experience. I earned multiple All-American titles with the support of my amazing coaches. During my last year of college, I found a newly fond appreciation for coaching. 
+
+I graduated back in December 2019 and continued to help coach the women’s wrestling team at life university. 
+
+While I was coaching and working in my field I constantly appreciated the development of my communication, problem-solving skills, and adaptability in many different scenarios. 
+
+I decided to make the shift to become a software engineer because I enjoy being challenged and enjoy the constantly evolving industry.</h2>
         </main>
     )
 }
