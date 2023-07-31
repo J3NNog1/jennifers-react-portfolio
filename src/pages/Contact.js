98d80ad8040da37a99b7e3bdcc9cc080cb27ce31
jennifer j <jenniferjuarezjj@gmail.com>
2023-07-30 22:02:07 -0400

    add contact information

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,9 @@ function Contact() {
     return (
         <main>
         <h1>Contact</h1>
+        <h2>Email: jenniferjuarezse@gmail.com</h2>
+        <h2>Phone number: (347) 399-7744</h2>
+        <h2>Linkedin: https://www.linkedin.com/in/jennifer-juarez44/ </h2>
         </main>
         
     )
